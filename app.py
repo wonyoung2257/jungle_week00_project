4d14da3a9ffb5223b5e8f525964d4f2bf4f7e700
박준규 <wnsrb003@naver.com>
2022-03-29 14:55:09 +0900

    - MongoDB 연결 추가(mongo cloud 사용) - rank API 추가하여 성공 횟수로 유저 정렬하여 전달 가능하게 추가

--- app.py
+++ app.py
@@ -6,12 +6,38 @@ app = Flask(__name__)
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbexam
+# client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb+srv://test:test@cluster0.b9rhp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = client.junglewordle
+# data = {
+#   'name': '허원영',
+#   'answer': 'ㅇㅜㅓㄴㅇㅕㅇ',
+#   'img': 'https://ca.slack-edge.com/T01FZU4LB4Y-U038VG9V6M9-2ec7f2a14209-512'}
+# data = {
+#   'id': '아이유',
+#   'pwd': '1234',
+#   'cnt_success': '8',
+#   'cnt_fail': '5'
+# }
+# db.users.insert_one(data)
+userList = list(db.users.find({}))
 
 @app.route('/')
 def home():
   return render_template('index.html')
+
+@app.route('/rank', methods=['GET'])
+def rank_list():
+  rank = sorted(userList, key=lambda user: (user['cnt_success']), reverse=True)
+  rank_list = []
+  for data in rank:
+    rank_list.append({
+      'id': data['id'],
+      'cntSuccess': data['cnt_success'],
+      'failSuccess': data['cnt_fail']
+    })
+
+  return jsonify({'result': 'success', 'rank_list': rank_list})
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5000, debug =True)
