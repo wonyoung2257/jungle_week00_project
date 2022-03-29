1d563efb4fe04c0330941d03f4471efc8f208ef5
Won-young <dnjsdud2257@gmail.com>
2022-03-29 20:55:49 +0900

    테일윈드 적용 전

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 
-from unittest import result
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
-from bson.json_util import dumps
 import json
-from bson.objectid import ObjectId
 from pymongo import MongoClient
 # client = MongoClient('localhost', 27017)
 client = MongoClient('mongodb+srv://test:test@cluster0.b9rhp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
@@ -62,4 +59,4 @@ def add_count_fail():
   return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
-  app.run('0.0.0.0', port=5000, debug =True)
+  app.run('0.0.0.0', port=5001, debug =True)
