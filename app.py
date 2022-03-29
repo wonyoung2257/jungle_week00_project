3feb5982b5486b6512cf3efe9dd76a0c8954d3db
Won-young <dnjsdud2257@gmail.com>
2022-03-29 15:55:48 +0900

    - game 로직 개발 진행중

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ db = client.dbexam
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  return render_template('main.html')
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5000, debug =True)
